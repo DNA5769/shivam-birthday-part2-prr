f3ffca036cc7f6d475d223fc3805d114cf4b5d2d
DNA5769 <dennisthomas2002@gmail.com>
2020-10-22 22:20:17 +0300

    Basic setup of player, added gitignore and added player to main

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import pygame
 pygame.init()
 
+from player import Player
+
 WIDTH = 800
 HEIGHT = 450
 
@@ -14,13 +16,19 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Shivam's Walk")
 pygame.display.set_icon(icon)
 
+p = Player()
+
 running = True
 while running:
 	screen.blit(BG, (0,0))
 
+	p.draw(screen)
+
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
+
+	p.move()
 
 	clock.tick(60)
 	pygame.display.update()
---
+++ player.py
@@ -0,0 +1,24 @@
+import os
+import pygame
+
+class Player:
+	def __init__(self):
+		self.src = [pygame.image.load(os.path.join('images', 'sprites', f'walk{i}.png')) for i in range(8)]
+		
+		self.x = 20
+		self.y = 400
+		
+		self.ind = 0
+		self.speed = 10
+		self.dist = 0
+
+	def draw(self, screen):
+		screen.blit(self.src[self.ind], (self.x, self.y))
+
+	def move(self):
+		self.dist += 1
+		if self.dist == self.speed:
+			self.dist = 0
+			self.ind += 1
+			if self.ind == 8:
+				self.ind = 0
