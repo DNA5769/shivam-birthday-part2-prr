cf7ea1dec3e37f52b56c3d4633ea2c438cd228c4
DNA5769 <dennisthomas2002@gmail.com>
2020-10-22 15:47:33 +0300

    Setup basic pygame window

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+pygame.init()
+
+WIDTH = HEIGHT = 500
+
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+running = True
+while running:
+	screen.fill((0,0,0))
+
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
+
+	clock.tick(60)
+	pygame.display.update()
