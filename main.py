cfce1e77f74045412bf94f3bfd18e0db183cc070
DNA5769 <dennisthomas2002@gmail.com>
2020-10-22 16:21:11 +0300

    Added icon to window

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+import os
 import pygame
 pygame.init()
 
 WIDTH = HEIGHT = 500
 
+icon = pygame.image.load(os.path.join('images', 'icon.png'))
+
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Shivam's Walk")
+pygame.display.set_icon(icon)
 
 running = True
 while running:
