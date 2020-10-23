a935fbd4ede76c28995c5676ada7cea572cf842e
DNA5769 <dennisthomas2002@gmail.com>
2020-10-23 13:01:56 +0300

    Added shivam's head

--- player.py
+++ player.py
@@ -5,18 +5,23 @@ class Player:
 	def __init__(self, SCREEN_HEIGHT):
 		self.WIDTH = 92
 		self.HEIGHT = 100
+		self.HEAD_WIDTH = 48
+		self.HEAD_HEIGHT = 48
 		self.x = 20
-		self.y = 3*SCREEN_HEIGHT//4 - self.HEIGHT//2
+		self.y = 3*SCREEN_HEIGHT//4
 
 		self.src = [pygame.image.load(os.path.join('images', 'sprites', f'walk{i}.png')) for i in range(8)]
 		self.src = [pygame.transform.scale(img, (self.WIDTH, self.HEIGHT)) for img in self.src]
-		
+		self.head = pygame.image.load(os.path.join('images', 'player_head.png'))
+		self.head = pygame.transform.scale(self.head, (self.HEAD_WIDTH, self.HEAD_HEIGHT))
+
 		self.ind = 0
 		self.speed = 10
 		self.dist = 0
 
 	def draw(self, screen):
 		screen.blit(self.src[self.ind], (self.x, self.y))
+		screen.blit(self.head, (self.x+self.head.get_width()/2+5, self.y - 7.5))
 
 	def move(self):
 		self.dist += 1
